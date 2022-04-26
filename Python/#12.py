577b8be01eb60362745d9797ea2758abd22d13d9
DavidDaza2906 <daviddaza2906@gmail.com>
2022-04-25 20:43:33 -0500

    Ejercicio #12

--- Python/#12.py
+++ Python/#12.py
@@ -1,15 +1,28 @@
-from distutils.cygwinccompiler import Mingw32CCompiler
-from locale import DAY_2
 
 import math
 def timeDifference(day1,hour1,min1,sec1,day2,hour2,min2,sec2):
-    dayF = day2 - day1 
-    hourF = round((hour2-hour1)-0.5)
-    hourF = hourF%60
-    minF = min2 - min1
-    secF = sec2- sec1
-    return dayF, hourF, minF,secF
+    if sec2 >= sec1:
+        s = sec2-sec1
+    else: 
+        min2 -=1
+        sec2 +=60
+        s = sec2 -sec1
+    if min2 >= min1:
+        m = min2-min1
+    else:
+        hour2 -=1
+        min2 += 60
+        m = min2-min1
+    if hour2 >= hour1:
+        h = hour2-hour1
 
+    else:
+        day2 -=1
+        hour2+= 24
+        h= hour2-hour1
+    d = day2-day1
+    k.append('('+str(d)+' '+ str(h)+' '+str(m)+' '+str(s)+')')
+k = []
 n = int(input())
 for _ in range(n):
     a = input().split()
@@ -21,4 +34,5 @@ for _ in range(n):
     hour2 = int(a[5])
     min2 = int(a[6])
     sec2 = int(a[7])
-    print(timeDifference(day1,hour1,min1,sec1,day2,hour2,min2,sec2))
+    timeDifference(day1,hour1,min1,sec1,day2,hour2,min2,sec2)
+print(' '.join(k))
--- Python/pruebas.py
+++ Python/pruebas.py
@@ -1,14 +1,2 @@
-n= int(input("Ingrese el número de asteriscos de la base de los triangulos: "))
-F = []
-for i in range(n):
-  string = ""
-  for j in range(i):
-    string += (" ")
-  for j in range(n - i):
-    string += ("* ")
-  for j in range(i):
-    string += (" ")
-  F.append(string)
-
-for i in range(n):
-  print(F[n-i-1] + F[i])
+a= 3%60
+print(a)
